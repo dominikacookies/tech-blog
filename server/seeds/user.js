bf69e8c33e16146b763c390fcc9b9072f2ce1720
Dominika <domi_nika@hotmail.co.uk>
2021-06-05 21:49:13 +0100

    added user seed data

---
+++ server/seeds/user.js
@@ -0,0 +1,20 @@
+[
+  {
+    "username": "user1",
+    "password": "password123",
+    "first_name": "Alice",
+    "last_name": "Franz"
+  },
+  {
+    "username": "user2",
+    "password": "password456",
+    "first_name": "Bob",
+    "last_name": "Bar"
+  },
+  {
+    "username": "user4",
+    "password": "password789",
+    "first_name": "Fred",
+    "last_name": "Foo"
+  }
+]
