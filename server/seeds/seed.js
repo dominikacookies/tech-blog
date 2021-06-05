5c8ac133289851da458c6567fbfd2ba7a9b4326b
Dominika <domi_nika@hotmail.co.uk>
2021-06-05 22:08:16 +0100

    set up seed file

---
+++ server/seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require("../config/connection");
+const User = require("../models/User");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const users = require("./user");
+const posts = require("./post");
+const comments = require("./comment");
+
+const seed = async () => {
+  await sequelize.sync({force:true});
+
+  await User.bulkCreate(users);
+  console.info("seeded users");
+
+  await Post.bulkCreate(posts);
+  console.info("seeded posts");
+
+  await Comment.bulkCreate(comments);
+  console.info("seeded comments");
+
+  process.exit(0);
+};
+
+seed()
