49663e1310c8be0324e1dbf43981f452a0d6dd74
Dominika <domi_nika@hotmail.co.uk>
2021-06-05 21:57:33 +0100

    added post seed data

---
+++ server/seeds/post.js
@@ -0,0 +1,22 @@
+[
+  {
+    "title" : "Blog post 1",
+    "body" : "This is the first blog post",
+    "user_id" : "1",
+  },
+  {
+    "title" : "Blog post 2",
+    "body" : "This is the second blog post",
+    "user_id" : "1",
+  },
+  {
+    "title" : "Blog post 3",
+    "body" : "This is the third blog post",
+    "user_id" : "2",
+  },
+  {
+    "title" : "Blog post 4",
+    "body" : "This is the fourth blog post",
+    "user_id" : "3",
+  }
+]
