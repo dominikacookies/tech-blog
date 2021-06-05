c1e185132b4b0511ce7318717644292a9264d38d
Dominika <domi_nika@hotmail.co.uk>
2021-06-05 21:11:03 +0100

    set up user mdoel

---
+++ server/models/User.js
@@ -0,0 +1,54 @@
+const {Model, DataTypes} = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const hooks = require("../hooks")
+const sequelize = require("../config/connection");
+
+const options = {
+  hooks,
+  sequelize,
+  modelName: "user",
+  timestamps: true,
+  underscored: true,
+  freezeTableName: true,
+};
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      unique: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+class User extends Model {
+  async isPasswordValid(password) {
+    const isValid = await bcrypt.compare(password, this.password);
+
+    return isValid;
+  }
+}
+
+User.init(schema, options)
+
+module.exports = User;
