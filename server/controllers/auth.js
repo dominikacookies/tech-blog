820931dc0cb5c20f49dc67074842c0d5e4ca8091
Dominika <domi_nika@hotmail.co.uk>
2021-06-08 10:51:22 +0100

    successfully creates user and catches errors

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,34 @@
+const { User } = require("../models")
+
 const signup = async (req, res) => {
-  const {username, password, firstName : first_name, lastName : last_name} = req.body
-  console.log(username, password, first_name, last_name)
-  // req.session.isLoggedIn = true
-  // console.log(req.session.isLoggedIn)
-  // req.session.save(() => {
-  //   req.session.isLoggedIn = true
-  // })
-  res.json({})
+  try {
+    const {username, password, firstName : first_name, lastName : last_name} = req.body
+
+    if (username && password && first_name && last_name) {
+
+      await User.create({
+        username,
+        password,
+        first_name,
+        last_name
+      })
+
+      return res.status(200).json({
+        message: "User created successfully."
+      })
+    }
+
+    return res.status(400).json({
+      error: "Required values missing."
+    })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      error: "Failed to create user"
+    })
+    
+  }
 }
 
 const login = async (req, res) => {
@@ -14,6 +36,11 @@ const login = async (req, res) => {
 }
 
 const logout = async (req, res) => {
+    // req.session.isLoggedIn = true
+  // console.log(req.session.isLoggedIn)
+  // req.session.save(() => {
+  //   req.session.isLoggedIn = true
+  // })
   console.log("logout")
 }
 
