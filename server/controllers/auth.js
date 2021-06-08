77b41f605164390d44ba3c9c327f733e1d7987d8
Dominika <domi_nika@hotmail.co.uk>
2021-06-08 10:35:46 +0100

    pulls out user data from req

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,19 @@
-const signup = () => {
-  console.log("singup")
+const signup = async (req, res) => {
+  const {username, password, firstName : first_name, lastName : last_name} = req.body
+  console.log(username, password, first_name, last_name)
+  // req.session.isLoggedIn = true
+  // console.log(req.session.isLoggedIn)
+  // req.session.save(() => {
+  //   req.session.isLoggedIn = true
+  // })
+  res.json({})
 }
 
-const login = () => {
+const login = async (req, res) => {
   console.log("login")
 }
 
-const logout = () => {
+const logout = async (req, res) => {
   console.log("logout")
 }
 
