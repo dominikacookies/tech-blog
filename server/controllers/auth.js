88b95e25b924e7ae87dc8e973ca30742d0ba545d
Dominika <domi_nika@hotmail.co.uk>
2021-06-08 12:23:48 +0100

    logs in and out

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,6 @@
+//remove later
+const bcrypt = require("bcrypt");
+
 const { User } = require("../models")
 
 const signup = async (req, res) => {
@@ -32,16 +35,63 @@ const signup = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  console.log("login")
+  try {
+    const { username , password } = req.body
+
+    if (!username || !password) {
+      return res.status(404).json({
+        error: "Required values missing."
+      })
+    }
+    
+    const userData = await User.findOne({where : { username }})
+    // why does this not work?
+    // user.get({ plain: true })
+
+    if (!userData) {
+      return res.status(404).json({
+        error: "User does not exist"
+      })
+    }
+
+    const {dataValues : user} = data
+  
+    // when trying to do this via model method it says that it doesn't exist
+    const matchResult = await bcrypt.compare(password, user.password);
+
+    if (!matchResult) {
+      return res.status(404).json({
+        error: "Password invalid."
+      })
+    }
+
+    req.session.user = {
+      id: user.id,
+      username: user.username,
+      firstName: user.first_name,
+      lastName: user.last_name,
+    }
+  
+    req.session.loggedIn = true
+  
+    return res.status(200).json({
+      message: "Log in successful."
+    })
+  
+  } catch (error) {
+    return res.status(500).json({
+      error: "Unable to login. Please try again later."
+    })
+  }
 }
 
+
 const logout = async (req, res) => {
-    // req.session.isLoggedIn = true
-  // console.log(req.session.isLoggedIn)
-  // req.session.save(() => {
-  //   req.session.isLoggedIn = true
-  // })
-  console.log("logout")
+  console.log(req.session)
+
+  delete req.session.loggedIn
+  delete req.session.user
+  console.log(req.session)
 }
 
 module.exports = {
