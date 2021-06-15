f3eb892a8380b34aa086143cc235a59980613f58
Dominika <domi_nika@hotmail.co.uk>
2021-06-15 11:26:12 +0100

    added placeholder fn for updating and deleting posts

--- server/controllers/html/private.js
+++ server/controllers/html/private.js
@@ -2,16 +2,26 @@ const renderDashboard = () => {
   console.log("Dashboard")
 }
 
-const renderEditPostPage = () => {
+const renderEditPostPage = (req, res) => {
   console.log("Edit post")
 }
 
-const renderPostPage = () => {
+const renderPostPage = (req, res) => {
   console.log("Post")
+}
+
+const updatePost = async (req, res) => {
+  console.log("update")
+}
+
+const deletePost = async (req, res) => {
+  console.log("delete")
 }
 
 module.exports = {
   renderDashboard,
   renderEditPostPage,
-  renderPostPage
+  renderPostPage,
+  updatePost,
+  deletePost
 }
