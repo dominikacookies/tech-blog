f9fdbc7f7ce63296e96caf1f7377c474a2930da5
Dominika <domi_nika@hotmail.co.uk>
2021-06-15 11:27:25 +0100

    added routes for updating and deleting posts

--- server/routes/html/private/index.js
+++ server/routes/html/private/index.js
@@ -1,11 +1,14 @@
 const { Router } = require("express")
 
-const {  renderDashboard, renderEditPostPage, renderPostPage} = require("../../../controllers/html/private")
+const {  renderDashboard, renderEditPostPage, renderPostPage, updatePost, deletePost} = require("../../../controllers/html/private");
 
 const router = Router();
 
 router.get("/dashboard", renderDashboard);
-router.get('/dashboard/edit/:id', renderEditPostPage);
+router.get('/dashboard/post/:id', renderEditPostPage);
 router.get('/post/:id', renderPostPage);
+
+router.post('/dashboard/post/:id', updatePost)
+router.delete('/dashboard/post/:id', deletePost)
 
 module.exports = router
