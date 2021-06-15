ab5870bf47ef4cac29d2d9b6d677c0689213eb9b
Dominika <domi_nika@hotmail.co.uk>
2021-06-15 11:26:48 +0100

    added colones for clarity

--- server/routes/html/index.js
+++ server/routes/html/index.js
@@ -6,7 +6,7 @@ const authenticator = require("../../middleware/authenticator");
 
 const router = Router();
 
-router.use(publicRoutes)
-router.use(authenticator, privateRoutes)
+router.use(publicRoutes);
+router.use(authenticator, privateRoutes);
 
 module.exports = router
