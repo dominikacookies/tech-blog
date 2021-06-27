e9f1f5b8abbbe106e6f1777aacfbd04117ee2fc9
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 22:11:27 +0100

    updated bulk creat fn to map through each user

--- server/hooks/index.js
+++ server/hooks/index.js
@@ -1,8 +1,16 @@
 const bcrypt = require("bcrypt");
 
-const beforeBulkCreate = async (user) => {
-  user.password = await bcrypt.hash(user.password, 10)
-}
+const beforeBulkCreate = async (users) => {
+  const hashPasswords = users.map((user) => {
+    return bcrypt.hash(user.password, 10);
+  });
+
+  const passwords = await Promise.all(hashPasswords);
+
+  passwords.forEach((password, index) => {
+    users[index].password = password;
+  });
+};
 
 const beforeCreate = async (user) => {
   user.password = await bcrypt.hash(user.password, 10)
