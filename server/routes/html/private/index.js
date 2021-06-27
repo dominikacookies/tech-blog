d13c7378be63ad59a9ebd427fd7d4aa3e8cebb3e
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 08:20:32 +0100

    moved update and edit routs to api

--- server/routes/html/private/index.js
+++ server/routes/html/private/index.js
@@ -1,14 +1,11 @@
 const { Router } = require("express")
 
-const {  renderDashboard, renderEditPostPage, renderPostPage, updatePost, deletePost} = require("../../../controllers/html/private");
+const { renderDashboard, renderEditPostPage, renderPostPage} = require("../../../controllers/html/private");
 
 const router = Router();
 
 router.get("/dashboard", renderDashboard);
 router.get('/dashboard/post/:id', renderEditPostPage);
 router.get('/post/:id', renderPostPage);
-
-router.post('/dashboard/post/:id', updatePost)
-router.delete('/dashboard/post/:id', deletePost)
 
 module.exports = router
