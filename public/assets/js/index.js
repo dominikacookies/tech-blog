b72f826e687080c9ce447f728cae4863cf571415
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 10:21:21 +0100

    logs in user and appends appr err msgs

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,47 @@
 console.log("hello to client")
 
-const submitLoginForm = (event) => {
+const submitLoginForm = async (event) => {
   event.preventDefault()
-  console.log("submitting")
+  const username = $("#username").val()
+  const password = $("#password").val()
+
+  if (username === "" || password === "") {
+    $(".error-message").remove()
+    $(event.target).parent().prepend(`
+    <p class="error-message text-center"> You must enter a username and password to log in. </p>
+    `)
+    return
+  }
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify({
+      username,
+      password
+    }),
+  };
+
+  const {status} = await fetch("/auth/login", options);
+
+  if (status === 200 ) {
+    window.location.replace("/dashboard")
+  } else if (status > 399 && status < 500) {
+    $(".error-message").remove()
+    $(event.target).parent().prepend(`
+    <p class="error-message text-center"> Username or password is incorrect. </p>
+    `)
+    return
+  } else {
+    $(".error-message").remove()
+    $(event.target).parent().prepend(`
+    <p class="error-message text-center"> Oops! We're unable to log you in right now. Please try again later. </p>
+    `)
+    return
+  }
 }
 
 $("#login-submit").on("click", submitLoginForm)
