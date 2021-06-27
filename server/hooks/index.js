c4bf74fa353d61d233dbb9b233847d60751a484c
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 21:48:51 +0100

    added logic for hashing password b4 bul create

--- server/hooks/index.js
+++ server/hooks/index.js
@@ -1,7 +1,11 @@
 const bcrypt = require("bcrypt");
 
+const beforeBulkCreate = async (user) => {
+  user.password = await bcrypt.hash(user.password, 10)
+}
+
 const beforeCreate = async (user) => {
   user.password = await bcrypt.hash(user.password, 10)
 }
 
-module.exports = { beforeCreate }
+module.exports = { beforeCreate, beforeBulkCreate }
