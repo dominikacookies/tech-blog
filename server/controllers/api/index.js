380b38fadc2f003b7f5f64e76882655f30ba640b
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 09:04:14 +0100

    creates and upddates comments

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,4 +1,4 @@
-const { Post } = require("../../models")
+const { Post, Comment } = require("../../models")
 
 const createPost = async (req, res) => {
   try {
@@ -18,10 +18,9 @@ const createPost = async (req, res) => {
 
     return res.status(200).json({
       message: "A new post has been successfully created",
-      category: newPost,
+      post: newPost,
     })
   } catch (error) {
-    console.log(error)
     return res.status(500).json({
       error: "Cannot create post at this time."
     })
@@ -105,11 +104,73 @@ const deletePost = async (req, res) => {
 }
 
 const createComment = async (req, res) => {
-  console.log("updating")
+  try {
+
+    const {id : userId} = req.session.user
+    const {message, postId} = req.body
+    
+    if (!message || !postId) {
+      return res.status(404).json({
+        error: "Required values missing."
+      })
+    }
+  
+    const newComment = await Comment.create({
+      message,
+      user_id : userId,
+      post_id : postId
+    })
+
+    return res.status(200).json({
+      message: "Update successful",
+      comment: newComment
+    })
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: "Couldn't create comment at this time."
+    })
+  
+  }
 }
 
 const updateComment = async (req, res) => {
-  console.log("updating")
+  try {
+
+    const {id} = req.params
+    const {message} = req.body
+
+    if (!message) {
+      return res.status(404).json({
+        error: "Required values missing.",
+      });
+    }
+
+    const updateStatus = await Comment.update({
+        message
+      },
+      {
+        where: {id}
+      })
+
+
+    if (!updateStatus[0]) {
+      return res.status(404).json({
+        error: "Comment doesn't exist",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Update successful"
+    })
+    
+    
+  } catch (error) {
+    res.status(500).json({
+      message: "Couldn't update comment at this time"
+    })
+  }
 }
 
 const deleteComment = async (req, res) => {
