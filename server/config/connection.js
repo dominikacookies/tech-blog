dca5e03945f44b2c6d7d49211b3f2db88ccf1f50
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 08:19:08 +0100

    replaced processs env with vars as wasn't connecting

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,9 +7,13 @@ const dbOptions = {
   logging: false,
 }
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+const dbName = "tech_blog_db"
+const dbUser = "root";
+const dbPassword = "password";
+
+// const dbName = process.env.DB_NAME;
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
 
 let connection;
 
@@ -18,5 +22,6 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = new sequelize(dbName, dbUser, dbPassword, dbOptions);
 }
+
 
 module.exports = connection;
