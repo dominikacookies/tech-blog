4a2a65a006b5d0306acfc8aa9bd9c093745107f7
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 08:58:43 +0100

    added authentiocator to api route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,12 @@ const {Router} = require("express");
 const authRoutes = require("./auth");
 const apiRoutes = require("./api")
 const htmlRoutes = require("./html")
+const authenticator = require("../middleware/authenticator");
 
 const router = Router();
 
 router.use("/auth", authRoutes);
-router.use("/api", apiRoutes);
+router.use("/api", authenticator, apiRoutes);
 router.use("/", htmlRoutes);
 
 module.exports = router
