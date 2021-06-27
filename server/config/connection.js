97bb1a2b90a887fe5e72abbc12f0d946e22942ec
Dominika <domi_nika@hotmail.co.uk>
2021-06-27 22:11:34 +0100

    changed vars to env

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,10 +7,6 @@ const dbOptions = {
   logging: false,
 }
 
-// const dbName = "tech_blog_db"
-// const dbUser = "root";
-// const dbPassword = "password";
-
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
